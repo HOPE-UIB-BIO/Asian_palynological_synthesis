ee2b2b4ce73109898c5f8ebdf04da39598420034
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-09-01 08:18:39 +0200

    update the fnc call with proportion data

--- R/01_Data_processing/PAP_estimation/03_RoC.R
+++ R/01_Data_processing/PAP_estimation/03_RoC.R
@@ -49,39 +49,62 @@ data_work_roc <-
         PAP_roc = purrr::pmap(
             .l = list(
                 pollen_counts, # ..1
-                levels, # ..2
-                age_uncertainty_matrix, # ..3
-                dataset_id # ..4
+                pollen_percentages, # ..2
+                orig_pollen_percentage, # ..3
+                levels, # ..4
+                age_uncertainty_matrix, # ..5
+
+                dataset_id # ..6
             ),
             .f = ~ {
                 message(
-                    msg = paste("dataset", ..4)
+                    msg = paste("dataset", ..6)
                 )
 
-                res <-
-                    RRatepol::fc_estimate_RoC(
-                        data_source_community = ..1,
-                        data_source_age = ..2,
-                        age_uncertainty = ..3,
-                        smooth_method = smoothing_method, # [config_criteria]
-                        smooth_N_points = min_points_smoothing, # [config_criteria]
-                        smooth_N_max = max_points_smoothing, # [config_criteria]
-                        smooth_age_range = age_range_smoothing, # [config_criteria]
-                        Working_Units = working_units_selection, # [config_criteria]
-                        bin_size = size_of_bin, # [config_criteria]
-                        Number_of_shifts = n_mowing_windows, # [config_criteria]
-                        bin_selection = which_level_select_in_bin, # [config_criteria]
-                        rand = n_rand, # [config_criteria]
-                        use_parallel = TRUE,
-                        standardise = TRUE,
-                        N_individuals = n_individuals_to_standardise, # [config_criteria]
-                        tranform_to_proportions = TRUE,
-                        DC = transformation_coef, # [config_criteria]
-                        interest_threshold = age_max, # [config_criteria]
-                        time_standardisation = size_of_bin # [config_criteria]
-                    )
+                if (
+                    ..3 == TRUE
+                ) {
+                    data_sel <- ..2 %>%
+                        dplyr::mutate(
+                            dplyr::across(
+                                tidyselect:::where(is.numeric), ~ .x / 100
+                            )
+                        )
+                } else {
+                    data_sel <- ..1
+                }
 
-                return(res)
+                try(
+                    roc_res <-
+                        RRatepol::fc_estimate_RoC(
+                            data_source_community = data_sel,
+                            data_source_age = ..4,
+                            age_uncertainty = ..5,
+                            smooth_method = smoothing_method, # [config_criteria]
+                            smooth_N_points = min_points_smoothing, # [config_criteria]
+                            smooth_N_max = max_points_smoothing, # [config_criteria]
+                            smooth_age_range = age_range_smoothing, # [config_criteria]
+                            Working_Units = working_units_selection, # [config_criteria]
+                            bin_size = size_of_bin, # [config_criteria]
+                            Number_of_shifts = n_mowing_windows, # [config_criteria]
+                            bin_selection = which_level_select_in_bin, # [config_criteria]
+                            rand = n_rand, # [config_criteria]
+                            use_parallel = TRUE,
+                            standardise = !..3,
+                            N_individuals = n_individuals_to_standardise, # [config_criteria]
+                            tranform_to_proportions = !..3,
+                            DC = transformation_coef, # [config_criteria]
+                            interoc_rest_throc_reshold = age_max, # [config_criteria]
+                            time_standardisation = size_of_bin # [config_criteria]
+                        ),
+                    silent = TRUE
+                )
+
+                if (!exists("roc_res")) {
+                    roc_res <- NA
+                }
+
+                return(roc_res)
             }
         )
     )
@@ -97,7 +120,7 @@ data_roc <-
 readr::write_rds(
     data_roc,
     here::here(
-        "Data/Processed/RoC/Data_roc_2022-07-30.rds"
+        "Data/Processed/RoC/Data_roc_2022-07-31.rds"
     ),
     compress = "gz"
 )
