873920fb96e051e44e6c604e6cfae93c3c6c8433
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-09-19 15:51:16 +0200

    add all variants of same var names

--- R/Functions/rename_variable_names.R
+++ R/Functions/rename_variable_names.R
@@ -9,9 +9,12 @@ rename_variable_names <-
                     var_name == "n2_divided_by_n1" ~ "N2 divided by N1",
                     var_name == "n1_divided_by_n0" ~ "N1 divided by N0",
                     var_name == "ROC" ~ "RoC",
-                    var_name == "Peak" ~ "Peak-points",
+                    var_name == "peakpoints" ~ "Peak-points",
                     var_name == "dcca_axis_1" ~ "DCCA1",
+                    var_name == "dcca" ~ "DCCA1",
                     var_name == "dca_axis_1" ~ "DCA1",
+                    var_name == "dca" ~ "DCA1",
+                    var_name == "mvrt" ~ "MVRT",
                     TRUE ~ var_name
                 )
             ) %>%
